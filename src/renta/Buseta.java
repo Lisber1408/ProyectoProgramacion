c759eca1fd1955a584e36e7bd80fc3f728ffdbc0
PRINCIPAL-PC <PRINCIPAL-PC@PRINCIPAL-PC>
2016-11-21 21:22:32 -0500

    Esta clase la extendi a la superClase RentaVehiculo, y ejecute los metodos abstractos de su superclase.

--- src/renta/Buseta.java
+++ src/renta/Buseta.java
@@ -9,6 +9,27 @@ package renta;
  *
  * @author LISBER
  */
-public class Buseta {
+public class Buseta extends RentaVehiculo{
+
+    public Buseta(String nombre, String apellido, String cedula, 
+            String numTelefono, int plazo) {
+        super(nombre, apellido, cedula, numTelefono, plazo);
+    }
+
+    @Override
+    public double CalcularCosto() {
+         return super.getPlazo()*70.0;
+    }
+
+    @Override
+    public double garantia() {
+        return CalcularCosto()/2;
+    }
+
+    @Override
+    public String descripcion() {
+        return "";
+    }
+    
     
 }
