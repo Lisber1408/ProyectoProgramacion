e2991d13cc26e8a9dee1bfac447fcaf89505f390
PRINCIPAL-PC <PRINCIPAL-PC@PRINCIPAL-PC>
2016-11-21 21:21:39 -0500

    Esta clase la extendi a la superClase RentaVehiculo, y ejecute los metodos abstractos de su superclase.

--- src/renta/VehiculoFamiliarMediano.java
+++ src/renta/VehiculoFamiliarMediano.java
@@ -9,6 +9,22 @@ package renta;
  *
  * @author LISBER
  */
-public class VehiculoFamiliarMediano {
+public class VehiculoFamiliarMediano extends RentaVehiculo{
+
+    //cosntructor
+    public VehiculoFamiliarMediano(String nombre, String apellido, 
+            String cedula, String numTelefono, int plazo) {
+        super(nombre, apellido, cedula, numTelefono, plazo);
+    }
+
+    @Override
+    public double CalcularCosto() {
+        return super.getPlazo()*45;
+    }
+
+    @Override
+    public String descripcion() {
+        return "";
+    }
     
 }
