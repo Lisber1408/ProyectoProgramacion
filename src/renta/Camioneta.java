aa318cbdd11d71372be1d280bc39a4d7a25481ff
PRINCIPAL-PC <PRINCIPAL-PC@PRINCIPAL-PC>
2016-11-21 21:22:05 -0500

    Esta clase la extendi a la superClase RentaVehiculo, y ejecute los metodos abstractos de su superclase.

--- src/renta/Camioneta.java
+++ src/renta/Camioneta.java
@@ -9,6 +9,23 @@ package renta;
  *
  * @author LISBER
  */
-public class Camioneta {
+public class Camioneta extends RentaVehiculo{
+ 
+    public Camioneta(String nombre, String apellido, String cedula,
+            String numTelefono, int plazo) {
+        super(nombre, apellido, cedula, numTelefono, plazo);
+    }
+
+    @Override
+    public double CalcularCosto() {
+        return (super.getPlazo()*65); 
+    }
+
+
+    @Override
+    public String descripcion() {
+        return  "Camioneta: Tundra \nToyota \nTanque de cobustible 38 galones"
+                + " \nMotor de 4.6L V8 ";
+    }
     
 }
